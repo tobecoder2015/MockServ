2c7fe3dc77bffc6f427d0f384cdde2ff5c3d8a36
虾斌 <qingshan.wqs@alibaba-inc.com>
2017-07-19 14:34:27 +0800

    添加ssl支持

--- src/main/java/com/wing/ProxyStart.java
+++ src/main/java/com/wing/ProxyStart.java
@@ -5,12 +5,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import javax.net.ssl.SSLSocketFactory;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * 参考  http://www.java2s.com/Tutorial/Java/0490__Security/HTTPSServer.htm
+ */
 @Slf4j
 @Component
 public class ProxyStart implements CommandLineRunner{
@@ -19,7 +23,11 @@ public class ProxyStart implements CommandLineRunner{
 	public void run(String... strings) throws Exception {
 		int port = 9111;
 		ServerSocket serverSocket = null;
+		System.setProperty("javax.net.ssl.trustStore", "clienttrust");
+		SSLSocketFactory ssf = (SSLSocketFactory) SSLSocketFactory.getDefault();
+
 		ExecutorService executorServicePool= Executors.newFixedThreadPool(50);
+		Socket s = ssf.createSocket("127.0.0.1", 8888);
 
 		try {
 			serverSocket = new ServerSocket(port);
